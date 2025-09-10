439677572f847627f41c7c4953133deeff5b1aae
carlos-araujo <araujopco@gmail.com>
2025-09-10 17:28:29 -0600

    first app

---
+++ first_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          colors: [Colors.deepPurple, Colors.purpleAccent],
+        ),
+      ),
+    ),
+  );
+}
